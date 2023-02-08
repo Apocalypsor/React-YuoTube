94863e495861b7c88551b80be837361f6cb60643
Apocalypsor <contact@changqixiao.com>
2023-02-07 21:42:48 -0500

    Add like content

--- frontend/src/component/common/LikeButton.js
+++ frontend/src/component/common/LikeButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
 import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import {getUser} from "../../tool";
 
 const styles = {
     icons: {
@@ -12,14 +13,27 @@ const styles = {
     }
 };
 
-function LikeButton({userId, videoId, size = "large"}) {
+function LikeButton({videoId, size = "large"}) {
+    const [userId, setUserId] = React.useState(null);
     const [liked, setLiked] = React.useState(false);
     const [clickLike, setClickLike] = React.useState(false);
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const res = await getUser();
+            if (res) {
+                setUserId(res.email);
+            }
+        }
+
+        fetchData();
+    }, []);
 
     function handleClick() {
         setClickLike(true);
     }
 
+    if (!userId) return null;
 
     return liked ? (<ThumbUpAltIcon fontSize={size} sx={{...styles.icons, color: "red"}} onClick={handleClick}/>) : (
         <ThumbUpOutlinedIcon fontSize={size} sx={{...styles.icons, color: "gray"}} onClick={handleClick}/>);
